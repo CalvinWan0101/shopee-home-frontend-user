62536530e5865f0aef0b49e46289a46264a50349
balaOuO <evictor92@gmail.com>
2023-11-12 15:21:03 +0800

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+//import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
